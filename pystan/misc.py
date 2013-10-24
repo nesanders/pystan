aed45e3fbad07bbe2307a628f52a9abaf84d1ed2
Allen Riddell <abr@ariddell.org>
2013-10-24 15:36:47 -0400

    Mock various things so readthedocs can build

--- pystan/misc.py
+++ pystan/misc.py
@@ -36,10 +36,9 @@ import time
 import warnings
 
 import numpy as np
-try:
-    from scipy.stats.mstats import mquantiles
-except ImportError:
-    from pystan.external.scipy.mstats import mquantiles
+
+# READTHEDOCS needs this change
+from pystan.external.scipy.mstats import mquantiles
 
 import pystan.chains as chains
 from pystan.constants import (MAX_UINT, sampling_algo_t, optim_algo_t,
